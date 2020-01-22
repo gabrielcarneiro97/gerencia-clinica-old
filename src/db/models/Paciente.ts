ce6d3417221bdb445a315c8fb8cc3c20589bb1d6
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-01-22 11:24:51 -0300

    criando os models

--- src/db/models/Paciente.ts
+++ src/db/models/Paciente.ts
@@ -1,1 +1,39 @@
-export default function a() {}
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../connect';
+
+import Endereco from './Endereco';
+
+export default class Paciente extends Model {
+  public id!: number;
+  public cpf!: string | null;
+  public nome!: string | null;
+  public sexo!: string | null;
+  public nascimento!: Date | null;
+  public enderecoId!: number | null;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Paciente.init({
+  id: {
+    type: new DataTypes.INTEGER.UNSIGNED(),
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  cpf: new DataTypes.STRING(),
+  nome: new DataTypes.STRING(),
+  sexo: new DataTypes.STRING(1),
+  nascimento: new DataTypes.DATE(),
+  enderecoId: {
+    type: new DataTypes.INTEGER.UNSIGNED(),
+    references: {
+      model: Endereco,
+      key: 'id',
+    },
+  },
+}, {
+  tableName: 'pacientes',
+  modelName: 'paciente',
+  sequelize,
+});
